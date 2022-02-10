50a49dbc858e09272f6d378fcb4b9fc62d172d16
C0mbed <matt@farncombe.co>
2022-02-10 09:01:04 -0700

    commit tree 56f52c5def52cdbc41175fd921e9bca6db6cbe17

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(arr, str) {
+    return arr.filter(word => word.toLowerCase() == str.toLowerCase());
+}
+
+function fuzzyMatch(arr, str) {
+   return arr.filter(word => word.toLowerCase().startsWith(str.toLowerCase()))
+}
+
+function matchName(obj, str) {
+    return obj.filter(element => element.name.toLowerCase() == str.toLowerCase())
+}
+
+
